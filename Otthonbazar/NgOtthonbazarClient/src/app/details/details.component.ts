90a9cf2aaa35eb5b7f96296990dc138a0fa1df8e
levoskaa <laurinyecz.levi@gmail.com>
2020-10-03 13:29:21 +0200

    Implement Details page

--- Otthonbazar/NgOtthonbazarClient/src/app/details/details.component.ts
+++ Otthonbazar/NgOtthonbazarClient/src/app/details/details.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Advertisement } from '../models';
 
 @Component({
   selector: 'app-details',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  advertisement: Advertisement;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.http.get<Advertisement>('api/advertisements/' + params['id'])
+        .subscribe(res => this.advertisement = res);
+    });
   }
-
 }
